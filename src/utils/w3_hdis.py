16bab4951876c41cf9719d8d6f485583b2235e34
cajnoj <jonjac@gmail.com>
2018-05-10 20:08:16 +0300

    rename

--- src/utils/w3_hdis.py
+++ src/utils/w3_hdis.py
@@ -4,7 +4,7 @@ import json
 
 HOSTNAME = "https://ropsten.infura.io/"
 CONTRACT_ADDRESS = Web3.toChecksumAddress("0xf8a15a2b41ab6a8165eed28c0ae97a26f8597092")
-ABI=json.load(open("utils/interface", "r"))
+ABI=json.load(open("solidity/interface", "r"))
 PUBLIC_KEY="0x802f75067b7502FCF18ffA6B43A143f37ac47fc2"
 PRIVATE_KEY="0xcc9da801e2338bbf9fe025e06fe55eb5a055651d270483b2161e8f9b011ba3c1"
 CHAIN_ID=3 # Ropsten
