d406344e8e3de22dd1aa6701529169077b8444e7
Dimid Duchovny <dimidd@gmail.com>
2018-05-10 18:31:18 +0300

    content: init getContentIds
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

---
+++ test/TestPurchaseContentContract.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.23;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+
+import "../contracts/HdisContent.sol";
+
+contract TestPurchaseContentContract is HdisContent {
+    function testPurchaseContent() public {
+        HdisContent hdisContent = HdisContent(DeployedAddresses.HdisContent());
+        string memory name = "test";
+        uint mediaId = 1;
+        uint mediaType = 2;
+        address creator = 0;
+        uint price = 10 wei;
+
+        uint id = hdisContent.addContent(name, mediaId, mediaType, creator, price);
+        (mediaId, mediaType, creator, price) = hdisContent.getContentById(id);
+        hdisContent.purchaseContent.value(price)(id);
+    }
+
+} 
