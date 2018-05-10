d04c76898b9722afccb1bd73cd2127eb00661cfc
david <davidvoler@gmail.com>
2018-05-10 14:37:41 +0300

    server with dashboard data

--- src/server.py
+++ src/server.py
@@ -1,29 +1,18 @@
 from tornado import ioloop, web
 import re
 import pymongo
-from handlers import  AccountHandler, TransactionHandler
-
-import config.settings
-
-from tornado.options import define, options, parse_command_line, parse_config_file
+from handlers.dashboard_handler import  DashboardHandler
 
 
 
 
 def api():
     return web.Application([
-        (r"/api/account", AccountHandler),
-        (r"/api/transaction", TransactionHandler),
+        (r"/api/dashboard", DashboardHandler),
     ])
 
 if __name__ == "__main__":
-    parse_command_line()
-    try:
-        parse_config_file('/conf/api_stub.conf')
-    except:
-        pass
     app = api()
-    app.listen(options.port)
-
-    print ("Payment running on port {}".format(options.port))
+    app.listen(8888)
+    print ("Payment running on port 8888")
     ioloop.IOLoop.current().start()
