9dbe5ab65d563f92a9c9bb828eee2d3b79b7185e
cajnoj <jonjac@gmail.com>
2018-05-10 21:25:05 +0300

    silly mistake

--- src/handlers/add_contributor_handler.py
+++ src/handlers/add_contributor_handler.py
@@ -9,5 +9,5 @@ class AddContributorHandler(web.RequestHandler):
     def get(self):
         media_id    = self.get_argument("media_id")
         contributor = self.get_argument("contributor", PUBLIC_KEY)
-        rs = addContributor(media_id, contributo, PRIVATE_KEY, PUBLIC_KEY)
+        rs = addContributor(media_id, contributor, PRIVATE_KEY, PUBLIC_KEY)
         self.write("Contributor added.")
